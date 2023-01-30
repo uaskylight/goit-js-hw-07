6bc55532f554bad23a454b50efdabcfd9b3bb88f
AlexT <uaskylight@gmail.com>
2023-01-30 12:56:18 +0200

    task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,26 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+const items = []
 
-const photo = document.querySelector(".gallery");
-const items = [];
+galleryItems.forEach(element => {
+	const galleryLink = document.createElement('a')
+	galleryLink.className = 'gallery__link'
+	galleryLink.href = element.original
+	const galleryImage = document.createElement('img')
+	galleryImage.className = 'gallery__image'
+	galleryImage.src = element.preview
+	galleryImage.setAttribute('title', element.description)
+	galleryImage.alt = element.description
 
-galleryItems.forEach((element) => {
-  const galleryLink = document.createElement("a");
-  galleryLink.className = "gallery__link";
-  galleryLink.href = element.original;
-  const galleryImage = document.createElement("img");
-  galleryImage.className = "gallery__image";
-  galleryImage.src = element.preview;
-  galleryImage.setAttribute("title", element.description);
-  galleryImage.alt = element.description;
+	galleryLink.append(galleryImage)
+	items.push(galleryLink)
+})
+gallery.append(...items)
 
-  galleryLink.append(galleryImage);
-  items.push(galleryLink);
-});
-photo.append(...items);
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+})
 
-new SimpleLightbox(".gallery a", {
-  captionDelay: 250,
-});
+
